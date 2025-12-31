59bd7169758fae2834bfd0d9158426910d412c4e
mizuyi <jogoizumi@gmail.com>
2025-12-31 13:14:05 +0900

    update: BackendにCORSの設定を追加

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello World from Backend" });
 });
